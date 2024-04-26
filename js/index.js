7dbbab71e020dd935834056af2a2db9b3a197eea
Asror Shodiyev <asrorshodiyev@Asror-2.local>
2024-04-26 11:02:52 +0500

    about page responsive end

--- js/index.js
+++ js/index.js
@@ -1,1 +1,17 @@
 "use strict";
+const menuBtn = document.querySelector('.menu-btn');
+const closeBtn = document.querySelector('.close-btn');
+const header__nav = document.querySelector('.header__nav');
+const closeBg = document.querySelector('.close-bg');
+menuBtn.addEventListener('click', () => {
+    header__nav.classList.add('active');
+    closeBg.classList.add('active');
+});
+closeBg.addEventListener('click', () => {
+    header__nav.classList.remove('active');
+    closeBg.classList.remove('active');
+});
+closeBtn.addEventListener('click', () => {
+    header__nav.classList.remove('active');
+    closeBg.classList.remove('active');
+});
---
+++ ts/index.ts
@@ -0,0 +1,16 @@
+const menuBtn = document.querySelector('.menu-btn') as HTMLButtonElement
+const closeBtn = document.querySelector('.close-btn') as HTMLButtonElement
+const header__nav = document.querySelector('.header__nav') as HTMLElement
+const closeBg = document.querySelector('.close-bg') as HTMLDivElement
+menuBtn.addEventListener('click', () => {
+	header__nav.classList.add('active')
+	closeBg.classList.add('active')
+})
+closeBg.addEventListener('click', () => {
+	header__nav.classList.remove('active')
+	closeBg.classList.remove('active')
+})
+closeBtn.addEventListener('click', () => {
+	header__nav.classList.remove('active')
+	closeBg.classList.remove('active')
+})
