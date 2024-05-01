609864d3884ceadea0216cbe7becb2f064a3fe80
Asror Shodiyev <asrorshodiyev@Asror-2.local>
2024-05-01 11:44:11 +0500

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,109 @@
-const gulp = require('gulp');
+const gulp = require('gulp')
 const sass = require('gulp-sass')(require('sass'));
-const purgecss = require('gulp-purgecss');
+const rename = require('gulp-rename');
+const cleanCss = require('gulp-clean-css');
+const babel = require('gulp-babel')
+const uglify = require('gulp-uglify')
+const concat = require('gulp-concat');
+const sourceMap = require('gulp-sourcemaps');
+const autoprefixer = require('gulp-autoprefixer');
+const htmlmin = require('gulp-htmlmin');
+const size = require('gulp-size');
+const webp = require('gulp-webp');
+const postcss = require('gulp-postcss');
+const autoprefixer2 = require('autoprefixer');
 
-function styles() {
-    return gulp.src('scss/*.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./css'));
+const paths = {
+	html: {
+		src: '*.html',
+		dest: 'dist/',
+	},
+	css: {
+		src: 'scss/*.scss',
+		dest: 'dist/css/'
+	},
+	js: {
+		src: 'js/*.js',
+		dest: 'dist/js/'
+	},
+	images: {
+		src: 'img/*.{jpg,png}',
+		dest: 'dist/img/'
+	}
 }
 
-// function purge() {
-//     return gulp.src('css/*.css')
-//         .pipe(purgecss({
-//             content: ['*.html', 'js/*.js'] // HTML va JavaScript fayllari asosida tekshirish
-//         }))
-//         .pipe(gulp.dest('css'));
-// }
 
+// html minify 
+function html() {
+	return gulp.src(paths.html.src)
+		.pipe(htmlmin({
+			collapseWhitespace: true
+		}))
+		.pipe(size({
+			showFiles: true
+		}))
+		.pipe(gulp.dest(paths.html.dest))
+}
+// build styles
+function css() {
+	return gulp.src(paths.css.src)
+		.pipe(sourceMap.init())
+		.pipe(sass())
+		.pipe(autoprefixer({
+			cascade: false
+		}))
+		.pipe(postcss([autoprefixer2()]))
+		.pipe(cleanCss({
+			level: 2
+		}))
+		.pipe(rename({
+			basename: 'main',
+			suffix: '.min'
+		}))
+		.pipe(sourceMap.write('.'))
+		.pipe(size({
+			showFiles: true
+		}))
+		.pipe(gulp.dest(paths.css.dest))
+}
+
+// build Scripts 
+function js() {
+	return gulp.src(paths.js.src)
+		.pipe(sourceMap.init())
+		.pipe(babel({
+			presets: ['@babel/env']
+		}))
+		.pipe(uglify())
+		.pipe(concat('main.min.js'))
+		.pipe(sourceMap.write('.'))
+		.pipe(size({
+			showFiles: true
+		}))
+		.pipe(gulp.dest(paths.js.dest))
+}
+
+// optimize images and convert to webp format
+function images() {
+	return gulp.src(paths.images.src)
+		.pipe(webp())
+		.pipe(gulp.dest(paths.images.dest))
+		.pipe(size({
+			showFiles: true
+		}));
+}
+// watch tasks
 function watch() {
-    gulp.watch('scss/*.scss', styles);
-    // gulp.watch('css/*.css', purge);
+	gulp.watch(paths.css.src, css);
+	gulp.watch(paths.html.src, html);
+	gulp.watch(paths.js.src, js);
+	gulp.watch(paths.images.src, images);
 }
+const build = gulp.series(gulp.parallel(css, js, html, images), watch)
 
-exports.default = watch;
+exports.css = css;
+exports.js = js;
+exports.html = html;
+exports.images = images;
+exports.build;
+exports.default = build
